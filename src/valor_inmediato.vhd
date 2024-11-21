c327e0224fcee003cd057c96d622635da442bda3
AG-Elian <aramallog.elian@gmail.com>
2024-11-21 10:49:52 -0300

    Valor inmediato corregido

--- src/valor_inmediato.vhd
+++ src/valor_inmediato.vhd
@@ -10,12 +10,13 @@ entity valor_inmediato is
 end valor_inmediato;
 
 architecture arch of valor_inmediato is
-begin
-  with sel select
-  inmediato<=(31 downto 12=>instr(31))&instr(31 downto 20)    when "000", -- Tipo I
-            (31 downto 12=>instr(31))&instr(31 downto 25)&instr(11 downto 7) when "001", -- Tipo S
-            (31 downto 13=>instr(31))&instr(31 downto 25)&instr(11 downto 7)&"0" when "010", -- Tipo B
-            instr(21 downto 12)&(11 downto 0='0'>) when "011", --Tipo U
-            (31 downto 21=>instr(31))&instr(31 downto 12)&'0' when "100",
+  U1 : with sel select
+  inmediato<=(31 downto 11=>instr(31)&instr(30 downto 20)) when "001", -- Tipo I
+              (31 downto 11=>instr(31))&instr(30 downto 25)&instr(11 downto 7) when "010", -- Tipo s
+              (31 downto 12=>instr(31))&instr(7)&instr(30 downto 25)&instr(11 downto 8)&"0" when "011", -- Tipo B
+              instr(31 downto 20)&instr(19 downto 12)&(11 downto 0=>'0') when "100", -- Tipo U
+              (30 downto 20=>instr(31))&instr(19 downto 12)&instr(20)&instr(30 downto 25)&instr(24 downto 2)&"0" when "101", -- Tipo J
+              x"00000000" when others;
+              -- end U1
 
 end arch;
