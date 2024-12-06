b6b30c89b13db2a13e3e92324be770bc62d71486
AG-Elian <aramallog.elian@gmail.com>
2024-12-06 19:03:59 -0300

    Se establecen cambios en el simlador de registro de 32 bits

--- src/sim_registro32.vhd
+++ src/sim_registro32.vhd
@@ -9,24 +9,38 @@ end sim_registro32;
 architecture sim of sim_registro32 is
   component registro32 is
     port (
-      A : in  std_logic;
-      B : in  std_logic;
-      Y : out std_logic
+      clk, hab, reset: in  std_logic;
+      D : in std_logic_vector(31 downto 0); -- datos de entrada
+      Q : out std_logic_vector(31 downto 0) --datos de salida
     );
   end component; -- registro32
-  signal entradas : std_logic_vector (1 downto 0);
-  signal salida : std_logic;
+  signal clk, hab, reset : std_logic;
+  signal salida, entrada : std_logic_vector(31 downto 0);
 begin
   -- Dispositivo bajo prueba
-  dut : registro32 port map (A=>entradas(1),B=>entradas(0),Y=>salida);
+  dut : registro32 port map (D=>entrada,clk=>clk,hab=>hab,reset=>reset,Q=>salida);
+
+  reloj: process
+  begin
+    clk<='0';
+    wait for 1 ns;
+    clk<='1';
+    wait for 1 ns;
+    end process; -- fin del proceso de reloj
 
   excitaciones: process
-  begin
-    for i in 0 to (2**entradas'length)-1 loop
-      entradas <= std_logic_vector(to_unsigned(i,entradas'length));
-      wait for 1 ns;
-    end loop;
-    wait for 1 ns; -- Espera extra antes de salir
-    finish;
-  end process; -- excitaciones
+  variable aleatorio : aleatorio_t;
+  procedure sig_ciclo is
+    begin
+      wait until rising_edge(clk);
+      wait for 0.5 ns;
+      end procedure;
+      begin
+        for k in 0 to 10 loop
+          entrada <= aleatorio.genera_vector(32);
+          hab <=aleatorio.genera_bit;
+          sig_ciclo;
+          finish;
+      end process; -- excitaciones
+  
 end sim;
